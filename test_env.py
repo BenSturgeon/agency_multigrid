8bfeb629b6122b7dcb3ec2a1724d9ef057236e81
Leo Hyams <leohyams74@gmail.com>
2023-09-15 12:16:26 +0200

    added measures.py which includes method for immediate choice.

--- test_env.py
+++ test_env.py
@@ -1,12 +1,15 @@
 import gymnasium as gym
 import multigrid.envs
+import time
 
 env = gym.make('MultiGrid-Custom-v0', agents=1, render_mode='human', autoreset=True)
+delay_seconds = 0.5
 
 observations, infos = env.reset()
 while not env.is_done():
    # this is where you would insert your policy / policies
    actions = {agent.index: agent.action_space.sample() for agent in env.agents}
    observations, rewards, terminations, truncations, infos = env.step(actions)
+   time.sleep(delay_seconds)
 
 env.close()
