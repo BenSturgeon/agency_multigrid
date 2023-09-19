ab153c5ac131782c2b71f955ca499cd6b17f169b
ben sturgeon <bensturgeon@bens-MacBook-Pro.local>
2023-09-19 18:58:01 +0200

    Added modified step function. Still need to add new reward functions to measure empowerment.

--- multigrid/envs/constrained.py
+++ multigrid/envs/constrained.py
@@ -11,7 +11,7 @@ class ConstrainedEnv(MultiGridEnv):
          agent_start_dir = [Direction.right, Direction.right],
          joint_reward: bool = False,
          success_termination_mode: str = 'any',
-         max_steps: int = 100,
+         max_steps: int = 200,
          **kwargs):
       
       self.agent_start_pos = agent_start_pos
@@ -73,3 +73,50 @@ class ConstrainedEnv(MultiGridEnv):
       
 
 
+      def step(self, actions):
+      # Get the action for the second agent
+            action = actions[1]
+
+            # Get the current position and direction of the second agent
+            agent_pos = self.agents[1].state.pos
+            agent_dir = self.agents[1].state.dir
+
+            # Get the position in front of the second agent
+            front_pos = agent_pos + Direction.DIR_VEC[agent_dir]
+
+            # Check if the action is to move forward
+            if action == 'forward':
+                  # Get the object in front of the second agent
+                  front_obj = self.grid.get(*front_pos)
+
+                  # Check if the object is a goal
+                  if isinstance(front_obj, Goal):
+                        # Prevent the second agent from moving onto the goal square
+                        actions[1] = 'done'  # Replace the 'forward' action with the 'done' action
+
+            # Call the step method of the superclass to handle the other actions
+            obs, rewards, dones, infos = super().step(actions)
+
+            # Check if the second agent has reached the goal
+            if isinstance(self.grid.get(*self.agents[1].state.pos), Goal):
+                  # Move the second agent back to its previous position
+                  self.agents[1].state.pos = agent_pos
+                  self.agents[1].state.dir = agent_dir
+
+            return obs, rewards, dones, infos
+      
+      def _reward(self, i):
+            # Get the current agent
+            agent = self.agents[i]
+
+            # Check if the agent is the second agent
+            reward = 0
+            if i == 1:
+                  # Define the reward function for the second agent
+                  # For example, give a reward of -1 for each step to encourage the agent to reach the goal as quickly as possible
+                  reward = -1
+            else:
+                  # Define the reward function for the other agents
+                  reward = 1 - 0.9 * (self.step_count / self.max_steps)
+
+            return 
